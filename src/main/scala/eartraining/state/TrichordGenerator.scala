bb1fad246efa24e2790f64c082fdbf8ba7d17b01
Zoltan Tamasi <tzoltan84@gmail.com>
2018-09-16 16:39:18 +0200

    Refactored tonewheel out of state.

--- src/main/scala/eartraining/state/TrichordGenerator.scala
+++ src/main/scala/eartraining/state/TrichordGenerator.scala
@@ -2,7 +2,6 @@ package eartraining.state
 
 import com.thoughtworks.binding.Binding.Var
 import eartraining._
-import eartraining.ui._
 
 import scala.util.Random
 
@@ -19,9 +18,7 @@ case class ChangeOctave(octave: Int) extends TrichordGeneratorAction
 case class TrichordGeneratorState(rotation: Var[Rotation],
                                   octaveExplode: Var[OctaveExplode],
                                   triadCore: Var[TriadCore],
-                                  baseNote: Var[Note],
-                                  imageAndRotation: Var[(String, String)],
-                                  noteWheelPositions: Var[List[(NoteName, Int, Boolean)]])
+                                  baseNote: Var[Note])
 
 case class TrichordGenerator(delegator: RootAction => Root) extends RootOption {
 
@@ -29,9 +26,7 @@ case class TrichordGenerator(delegator: RootAction => Root) extends RootOption {
     rotation = Var(Rotation0),
     octaveExplode = Var(NotOctaveExploded),
     triadCore = Var(Major),
-    baseNote = Var(Note(C, 3)),
-    imageAndRotation = Var(("3-5-4", "0")),
-    noteWheelPositions = Var(getNoteWheel(Major, Rotation0, NotOctaveExploded, Note(C, 3)))
+    baseNote = Var(Note(C, 3))
   )
 
   def handleAction(action: RootAction): TrichordGenerator = {
@@ -44,13 +39,10 @@ case class TrichordGenerator(delegator: RootAction => Root) extends RootOption {
           noteName <- List(C, C_#, D, D_#, E, F, F_#, G, G_#, A, A_#)
           octave <- List(2, 3, 4)
         } yield Note(noteName, octave))
-        state.noteWheelPositions := getNoteWheel(state.triadCore.get, state.rotation.get, state.octaveExplode.get, state.baseNote.get)
         this
 
       case ChangeRotation(rotation: Rotation) =>
         state.rotation := rotation
-        state.imageAndRotation := getImageAndRotation(state)
-        state.noteWheelPositions := getNoteWheel(state.triadCore.get, state.rotation.get, state.octaveExplode.get, state.baseNote.get)
         this
 
       case ChangeOctaveExploded(enabled: OctaveExplode) =>
@@ -59,13 +51,10 @@ case class TrichordGenerator(delegator: RootAction => Root) extends RootOption {
 
       case ChangeTriadCore(triadCore: TriadCore) =>
         state.triadCore := triadCore
-        state.imageAndRotation := getImageAndRotation(state)
-        state.noteWheelPositions := getNoteWheel(state.triadCore.get, state.rotation.get, state.octaveExplode.get, state.baseNote.get)
         this
 
       case ChangeBaseNote(noteName: NoteName) =>
         state.baseNote := Note(noteName, state.baseNote.get.octave)
-        state.noteWheelPositions := getNoteWheel(state.triadCore.get, state.rotation.get, state.octaveExplode.get, state.baseNote.get)
         this
 
       case ChangeOctave(octave: Int) =>
@@ -83,51 +72,5 @@ case class TrichordGenerator(delegator: RootAction => Root) extends RootOption {
   }
 
   private def pullRandom[T](list: Seq[T]): T = list(Random.nextInt(list.length))
-
-  private def getImageAndRotation(state: TrichordGeneratorState): (String, String) = {
-    val triadCore: TriadCore = state.triadCore.get
-    val image = s"${triadCore match {
-      case StackedMinor2 => "1-1-10"
-      case Minor2PlusMajor2 => "1-2-9"
-      case Major2PlusMinor2 => "1-9-2"
-      case MinorMajor => "1-3-8"
-      case MinorMajorI => "1-8-3"
-      case Lyd => "1-5-6"
-      case Locr => "1-6-5"
-      case Minor7Plus6 => "2-2-8"
-      case Minor7With3 => "2-3-7"
-      case Minor7With5 => "2-7-3"
-      case LydSus2 => "2-4-6"
-      case AugSus2 => "2-6-4"
-      case Minor => "3-4-5"
-      case Major => "3-5-4"
-      case Augmented => "4-4-4"
-      case Diminished => "3-3-6"
-      case Major7Without5 => "1-7-4"
-      case Major7Without3 => "1-4-7"
-      case Stacked4s => "2-5-5"
-    }}.png"
-    val rotation = state.rotation.get match {
-      case Rotation0 => 0
-      case Rotation1 => triadCore.intervals._1 * 30
-      case Rotation2 => (triadCore.intervals._1 + triadCore.intervals._2) * 30
-    }
-
-    (image, s"transform: rotate(${rotation}deg)")
-  }
-
-  private def getNoteWheel(triadCore: TriadCore, rotation: Rotation, octaveExplode: OctaveExplode, baseNote: Note): List[(NoteName, Int, Boolean)] = {
-
-    val chord = Chord(triadCore, rotation, octaveExplode, baseNote)
-    val noteList = Function.chain(List.fill(11)((list: List[NoteName]) => list :+ NoteName.successor(list.last)))(List(baseNote.noteName))
-
-    (noteList zip Range(0, 12)).map { case (noteName, position) =>
-        if (Chord.notesOf(chord).map(note => note.noteName).contains(noteName)) {
-          (noteName, position, true)
-        } else {
-          (noteName, position, false)
-        }
-    }
-  }
 
 }
--- src/main/scala/eartraining/ui/TrichordGenerator.scala
+++ src/main/scala/eartraining/ui/TrichordGenerator.scala
@@ -9,129 +9,160 @@ import org.scalajs.dom.raw.{HTMLInputElement, HTMLSelectElement}
 
 object TrichordGeneratorUI extends StateToUI[TrichordGenerator] {
 
+  private def getImageAndRotation(triadCore: TriadCore, rotation: Rotation): (String, String) = {
+    val image = triadCore.getImage
+    val imageRotation = rotation match {
+      case Rotation0 => 0
+      case Rotation1 => triadCore.intervals._1 * 30
+      case Rotation2 => (triadCore.intervals._1 + triadCore.intervals._2) * 30
+    }
+
+    (image, s"transform: rotate(${imageRotation}deg)")
+  }
+
+  private def getNoteWheel(triadCore: TriadCore, rotation: Rotation, octaveExplode: OctaveExplode, baseNote: Note): List[(NoteName, Int, Boolean)] = {
+
+    val chord = Chord(triadCore, rotation, octaveExplode, baseNote)
+    val noteList = Function.chain(List.fill(11)((list: List[NoteName]) => list :+ NoteName.successor(list.last)))(List(baseNote.noteName))
+
+    (noteList zip Range(0, 12)).map { case (noteName, position) =>
+      if (Chord.notesOf(chord).map(note => note.noteName).contains(noteName)) {
+        (noteName, position, true)
+      } else {
+        (noteName, position, false)
+      }
+    }
+  }
+
   @dom
   def toUI(trichordGenerator: TrichordGenerator): Binding[Node] = {
     <div>
       <form onsubmit={(event:Event) => event.preventDefault()}>
-      <div class="row">
-        <div class="col">
-          <div class="form-group">
-            <label for="triadCoreSelect">Triad core</label>
-            <select class="form-control" id="triadCoreSelect" onchange={(event: Event) =>
-              val selectedValue = event.target.asInstanceOf[HTMLSelectElement].value
-              trichordGenerator.handleAction(ChangeTriadCore(TriadCore.fromString(selectedValue)))
-            }>
-              {
-                Constants(TriadCore.allTriadTypes: _*) map { triadCore =>
-                  <option selected={trichordGenerator.state.triadCore.bind == triadCore} value={triadCore.toString}>
-                    {triadCore.label}
+        <div class="row">
+          <div class="col">
+            <div class="form-group">
+              <label for="triadCoreSelect">Triad core</label>
+              <select class="form-control" id="triadCoreSelect" onchange={(event: Event) =>
+                val selectedValue = event.target.asInstanceOf[HTMLSelectElement].value
+                trichordGenerator.handleAction(ChangeTriadCore(TriadCore.fromString(selectedValue)))
+              }>
+                {
+                  Constants(TriadCore.allTriadTypes: _*) map { triadCore =>
+                    <option selected={trichordGenerator.state.triadCore.bind == triadCore} value={triadCore.toString}>
+                      {triadCore.label}
+                    </option>
+                  }
+                }
+              </select>
+            </div>
+          </div>
+          <div class="col">
+            <div class="form-group">
+              <label for="baseNoteSelect">Base note</label>
+              <select class="form-control" id="baseNoteSelect" onchange={(event: Event) =>
+                val selectedValue = event.target.asInstanceOf[HTMLSelectElement].value
+                trichordGenerator.handleAction(ChangeBaseNote(NoteName.fromString(selectedValue)))
+              }>
+                {
+                Constants(List(C, C_#, D, D_#, E, F, F_#, G, G_#, A, A_#, B): _*) map { note =>
+                  <option selected={trichordGenerator.state.baseNote.bind.noteName == note} value={note.toString}>
+                    {note.toString}
                   </option>
                 }
-              }
-            </select>
+                }
+              </select>
+            </div>
+          </div>
+          <div class="col">
+            <div class="form-group">
+              <label for="octaveSelect">Octave</label>
+              <select class="form-control" id="octaveSelect" onchange={(event: Event) =>
+                val selectedValue = event.target.asInstanceOf[HTMLSelectElement].value
+                trichordGenerator.handleAction(ChangeOctave(selectedValue.toInt))
+              }>
+                {
+                Constants(List(2, 3, 4): _*) map { octave =>
+                  <option selected={trichordGenerator.state.baseNote.bind.octave == octave} value={octave.toString}>
+                    {octave.toString}
+                  </option>
+                }
+                }
+              </select>
+            </div>
           </div>
         </div>
-        <div class="col">
-          <div class="form-group">
-            <label for="baseNoteSelect">Base note</label>
-            <select class="form-control" id="baseNoteSelect" onchange={(event: Event) =>
-              val selectedValue = event.target.asInstanceOf[HTMLSelectElement].value
-              trichordGenerator.handleAction(ChangeBaseNote(NoteName.fromString(selectedValue)))
-            }>
-              {
-              Constants(List(C, C_#, D, D_#, E, F, F_#, G, G_#, A, A_#, B): _*) map { note =>
-                <option selected={trichordGenerator.state.baseNote.bind.noteName == note} value={note.toString}>
-                  {note.toString}
-                </option>
-              }
-              }
-            </select>
+        <div class="row">
+          <div class="col">
+            <div class="form-group">
+              <label for="rotationSelect">Rotations</label>
+              <select class="form-control" id="rotationSelect" onchange={(event: Event) =>
+                val selectedValue = event.target.asInstanceOf[HTMLSelectElement].value
+                trichordGenerator.handleAction(ChangeRotation(Rotation.fromString(selectedValue)))
+              }>
+                <option value={Rotation0.toString} selected={ trichordGenerator.state.rotation.bind == Rotation0 }>1st position</option>
+                <option value={Rotation1.toString} selected={ trichordGenerator.state.rotation.bind == Rotation1 }>2nd position</option>
+                <option value={Rotation2.toString} selected={ trichordGenerator.state.rotation.bind == Rotation2 }>3rd position</option>
+              </select>
+            </div>
+          </div>
+          <div class="col">
+            Octave extraction
+            <input type="checkbox"
+                   checked={trichordGenerator.state.octaveExplode.bind == OctaveExploded}
+                   onchange={(event: Event) =>
+                     trichordGenerator.handleAction(
+                       ChangeOctaveExploded(OctaveExplode.fromBoolean(event.target.asInstanceOf[HTMLInputElement].checked))
+                     )
+                   }>
+            </input>
           </div>
         </div>
-        <div class="col">
-          <div class="form-group">
-            <label for="octaveSelect">Octave</label>
-            <select class="form-control" id="octaveSelect" onchange={(event: Event) =>
-              val selectedValue = event.target.asInstanceOf[HTMLSelectElement].value
-              trichordGenerator.handleAction(ChangeOctave(selectedValue.toInt))
-            }>
-              {
-              Constants(List(2, 3, 4): _*) map { octave =>
-                <option selected={trichordGenerator.state.baseNote.bind.octave == octave} value={octave.toString}>
-                  {octave.toString}
-                </option>
-              }
-              }
-            </select>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          <div class="form-group">
-            <label for="rotationSelect">Rotations</label>
-            <select class="form-control" id="rotationSelect" onchange={(event: Event) =>
-              val selectedValue = event.target.asInstanceOf[HTMLSelectElement].value
-              trichordGenerator.handleAction(ChangeRotation(Rotation.fromString(selectedValue)))
-            }>
-              <option value={Rotation0.toString} selected={ trichordGenerator.state.rotation.bind == Rotation0 }>1st position</option>
-              <option value={Rotation1.toString} selected={ trichordGenerator.state.rotation.bind == Rotation1 }>2nd position</option>
-              <option value={Rotation2.toString} selected={ trichordGenerator.state.rotation.bind == Rotation2 }>3rd position</option>
-            </select>
-          </div>
-        </div>
-        <div class="col">
-          Octave extraction
-          <input type="checkbox"
-                 checked={trichordGenerator.state.octaveExplode.bind == OctaveExploded}
-                 onchange={(event: Event) =>
-                   trichordGenerator.handleAction(
-                     ChangeOctaveExploded(OctaveExplode.fromBoolean(event.target.asInstanceOf[HTMLInputElement].checked))
-                   )
-                 }>
-          </input>
-        </div>
-      </div>
 
-      <hr/>
+        <hr/>
 
-      <div class="row justify-content-center">
-        <div id="note-wheel">
-          {
-          Constants(trichordGenerator.state.noteWheelPositions.bind: _*) map { case (noteName, pos, selected) =>
-            <span class={ s"note-label pos-${pos} ${if (selected) "selected" else ""}"}> {NoteName.toString(noteName)} </span>
-          }
-          }
-          <img src={trichordGenerator.state.imageAndRotation.bind._1} style={trichordGenerator.state.imageAndRotation.bind._2}></img>
-        </div>
-        Chord notes: <br />
-        {
-          Chord(trichordGenerator.state.triadCore.bind,
+        <div class="row justify-content-center">
+          <div id="note-wheel">
+            {
+              Constants(getNoteWheel(trichordGenerator.state.triadCore.bind,
                 trichordGenerator.state.rotation.bind,
                 trichordGenerator.state.octaveExplode.bind,
-                Note(trichordGenerator.state.baseNote.bind.noteName, trichordGenerator.state.baseNote.bind.octave)).toString
-        }
-      </div>
+                trichordGenerator.state.baseNote.bind): _*) map { case (noteName, pos, selected) =>
+                <span class={ s"note-label pos-${pos} ${if (selected) "selected" else ""}"}> {NoteName.toString(noteName)} </span>
+              }
+            }
+            {
+              val (image, rotation) = getImageAndRotation(trichordGenerator.state.triadCore.bind, trichordGenerator.state.rotation.bind)
+              <img src={image} style={rotation}></img>
+            }
+          </div>
+          Chord notes: <br />
+          {
+            Chord(trichordGenerator.state.triadCore.bind,
+                  trichordGenerator.state.rotation.bind,
+                  trichordGenerator.state.octaveExplode.bind,
+                  Note(trichordGenerator.state.baseNote.bind.noteName, trichordGenerator.state.baseNote.bind.octave)).toString
+          }
+        </div>
 
-      <hr/>
+        <hr/>
 
-      <div class="row justify-content-center">
-        <button onclick={(event: Event) => trichordGenerator.handleAction(PlayCurrentChord)   }>
-          Play Chord
-        </button>
-        <button onclick={(_: Event) => trichordGenerator.handleAction(Randomize) }>
-          Randomize
-        </button>
-      </div>
+        <div class="row justify-content-center">
+          <button onclick={(event: Event) => trichordGenerator.handleAction(PlayCurrentChord)   }>
+            Play Chord
+          </button>
+          <button onclick={(_: Event) => trichordGenerator.handleAction(Randomize) }>
+            Randomize
+          </button>
+        </div>
 
-      <hr/>
+        <hr/>
 
-      <div class="row justify-content-center">
-        <button onclick={(_: Event) => trichordGenerator.handleAction(BackToMenu)}>
-          Back
-        </button>
-      </div>
-    </form>
+        <div class="row justify-content-center">
+          <button onclick={(_: Event) => trichordGenerator.handleAction(BackToMenu)}>
+            Back
+          </button>
+        </div>
+      </form>
     </div>
 
   }
